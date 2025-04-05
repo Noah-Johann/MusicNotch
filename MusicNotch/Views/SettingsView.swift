13d665d31586f916fa38d58a6fc3e7353685ab4c
Noah <179856882+Noah-Johann@users.noreply.github.com>
2025-04-05 10:43:56 +0200

    Added slider info

--- MusicNotch/Views/SettingsView.swift
+++ MusicNotch/Views/SettingsView.swift
@@ -38,11 +38,11 @@ struct SettingsView: View {
                             Text("Quit")
                         }
                     } .buttonStyle(LuminareButtonStyle())
-                }
+                } .padding(.bottom, 14)
                 
                 
                 LuminareSection("Notch") {
-                } .padding(.bottom, 7)
+                } .padding(.bottom, 14)
                 
                 LuminareSection("About") {
                     aboutAppButton()
@@ -61,41 +61,46 @@ struct SettingsView: View {
                                 link: URL(string: "https://github.com/Noah-Johann/MusicNotch")!,
                                 image: Image("Github")
                     ) .frame( height: 60)
-                } .padding(.bottom, 7)
+                } .padding(.bottom, 14)
                 
                 LuminareSection("Acknowledgements") {
                     aboutLicenseButton(name: "Luminare",
                                        license: "BSD 3-Clause",
                                        link: URL(string: "https://github.com/MrKai77/Luminare/tree/main")!,
                                        image: "book"
-                    ) .frame( height: 50)
+                    ) .frame( height: 40)
                     aboutLicenseButton(name: "DynamicNotchKit",
                                        license: "MIT",
                                        link: URL(string: "https://github.com/MrKai77/DynamicNotchKit")!,
                                        image: "book"
-                    ) .frame( height: 50)
+                    ) .frame( height: 40)
                     aboutLicenseButton(name: "KeyboardShortcuts",
                                        license: "MIT",
                                        link: URL(string: "https://github.com/sindresorhus/KeyboardShortcuts")!,
                                        image: "book"
-                    ) .frame( height: 50)
+                    ) .frame( height: 40)
                     aboutLicenseButton(name: "LaunchAtLogin",
                                        license: "MIT",
                                        link: URL(string: "https://github.com/sindresorhus/LaunchAtLogin-Modern")!,
                                        image: "book"
-                    ) .frame( height: 50)
+                    ) .frame( height: 40)
                     aboutLicenseButton(name: "Defaults",
                                        license: "MIT",
                                        link: URL(string: "https://github.com/sindresorhus/Defaults")!,
                                        image: "book"
-                    ) .frame( height: 50)
+                    ) .frame( height: 40)
+                    aboutLicenseButton(name: "Custom Slider Control",
+                                       license: "MIT",
+                                       link: URL(string: "https://github.com/pratikg29/Custom-Slider-Control")!,
+                                       image: "book"
+                    ) .frame( height: 40)
                 }
                 
                 Text(Bundle.main.copyright)
                     .padding()
                     .font(.caption)
                     .foregroundStyle(.secondary)
-            } .padding(30)
+            }
         }
     }
     
