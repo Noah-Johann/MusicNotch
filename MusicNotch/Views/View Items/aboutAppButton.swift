a1f55efbde1a2993e33e3c6f1828ed7736db5cf7
Noah <179856882+Noah-Johann@users.noreply.github.com>
2025-04-19 13:27:15 +0200

    Changed file structure

---
+++ MusicNotch/Views/View Items/aboutAppButton.swift
@@ -0,0 +1,38 @@
+//
+//  aboutAppButton.swift
+//  MusicNotch
+//
+//  Created by Noah Johann on 04.04.25.
+//
+
+import SwiftUI
+import Luminare
+
+@ViewBuilder
+func aboutAppButton() -> some View {
+    Button {
+        copyInfo(text: "MusicNotch Version \(Bundle.main.appVersion!) (\(Bundle.main.appBuild!))")
+    } label: {
+        HStack(spacing: 12) {
+            Image(nsImage: NSApp.applicationIconImage)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 60, height: 60)
+
+
+            VStack(alignment: .leading) {
+                Text(Bundle.main.appName)
+                Text("Version: \(Bundle.main.appVersion!) (\(Bundle.main.appBuild!))")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+                
+            }
+
+            Spacer()
+        }
+        .padding(4)
+    }
+    .buttonStyle(LuminareCosmeticButtonStyle(Image(systemName: "clipboard")))
+}
+
+
