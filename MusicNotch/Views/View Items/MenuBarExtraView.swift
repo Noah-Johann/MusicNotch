7ab317760a6989e5cb037ac306c3e86361157ce3
Noah <179856882+Noah-Johann@users.noreply.github.com>
2025-04-22 16:09:24 +0200

    Updated app icons

---
+++ MusicNotch/Views/View Items/MenuBarExtraView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuBarExtraView.swift
+//  MusicNotch
+//
+//  Created by Noah Johann on 22.04.25.
+//
+
+import SwiftUI
+
+struct MenuBarExtraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuBarExtraView()
+}
