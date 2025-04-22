54c4416b4122f6baf3a7b68ae97532bc58693707
Noah <179856882+Noah-Johann@users.noreply.github.com>
2025-04-22 16:09:58 +0200

    Moved MenuBarExtraView

--- MusicNotch/MusicNotchApp.swift
+++ MusicNotch/MusicNotchApp.swift
@@ -39,60 +39,7 @@ struct MusicNotchApp: App {
     var body: some Scene {
         
         MenuBarExtra("MusicNotch", image: "notch.square", isInserted: $showMenuBarIcon) {
-            Section {
-                Button {
-                    spotifyPlayPause()
-                } label: {
-                    Image(systemName: "play.fill")
-                    Text("Play")
-                }
-                
-                Button {
-                    spotifyNextTrack()
-                } label: {
-                    Image(systemName: "forward.end.fill")
-                    Text("Next")
-                }
-                Button {
-                    spotifyLastTrack()
-                } label: {
-                    Image(systemName: "backward.end.fill")
-                    Text("Previous")
-                }
-                
-            }
-            Section {
-                Text("Version \(Bundle.main.appVersion!) (\(Bundle.main.appBuild!))")
-                    .foregroundStyle(.secondary)
-                
-                Button("About") {
-                    let aboutWindow = LuminareWindow(blurRadius: 20) {
-                        aboutView()
-                            .frame(width: 300, height: 380)
-                    }
-                    
-                    aboutWindow.center()
-                    aboutWindow.level = .floating
-                    aboutWindow.show()
-                }
-                
-                Button("Settings") {
-                    let settingsWindow = LuminareWindow(blurRadius: 20) {
-                        SettingsView()
-                            .frame(width: 500, height: 600)
-                    }
-                    
-                    settingsWindow.center()
-                    settingsWindow.show()
-                    
-                } .keyboardShortcut(.init(",", modifiers: [.command]))
-            }
-            Section {
-                Button("Quit", role: .destructive) {
-                    NSApp.terminate(nil)
-                } .keyboardShortcut("Q", modifiers: .command)
-                
-            }
+            MenuBarExtraView()
         }
         
     }
--- MusicNotch/Views/MenuBarExtraView.swift
+++ MusicNotch/Views/MenuBarExtraView.swift
@@ -6,10 +6,64 @@
 //
 
 import SwiftUI
+import Luminare
 
 struct MenuBarExtraView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Section {
+            Button {
+                spotifyPlayPause()
+            } label: {
+                Image(systemName: "play.fill")
+                Text("Play")
+            }
+            
+            Button {
+                spotifyNextTrack()
+            } label: {
+                Image(systemName: "forward.end.fill")
+                Text("Next")
+            }
+            Button {
+                spotifyLastTrack()
+            } label: {
+                Image(systemName: "backward.end.fill")
+                Text("Previous")
+            }
+            
+        }
+        Section {
+            Text("Version \(Bundle.main.appVersion!) (\(Bundle.main.appBuild!))")
+                .foregroundStyle(.secondary)
+            
+            Button("About") {
+                let aboutWindow = LuminareWindow(blurRadius: 20) {
+                    aboutView()
+                        .frame(width: 300, height: 380)
+                }
+                
+                aboutWindow.center()
+                aboutWindow.level = .floating
+                aboutWindow.show()
+            }
+            
+            Button("Settings") {
+                let settingsWindow = LuminareWindow(blurRadius: 20) {
+                    SettingsView()
+                        .frame(width: 500, height: 600)
+                }
+                
+                settingsWindow.center()
+                settingsWindow.show()
+                
+            } .keyboardShortcut(.init(",", modifiers: [.command]))
+        }
+        Section {
+            Button("Quit", role: .destructive) {
+                NSApp.terminate(nil)
+            } .keyboardShortcut("Q", modifiers: .command)
+            
+        }
     }
 }
 
