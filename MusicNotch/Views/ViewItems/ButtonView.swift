0eaf84bf4cec0994dd2aa290fc014cf28f85c2a4
Noah <179856882+Noah-Johann@users.noreply.github.com>
2025-04-26 18:44:31 +0200

    Moved Controlls to seperate file

---
+++ MusicNotch/Views/ViewItems/ButtonView.swift
@@ -0,0 +1,98 @@
+//
+//  ButtonView.swift
+//  MusicNotch
+//
+//  Created by Noah Johann on 26.04.25.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        //Controls
+        HStack {
+            
+            //Shuffle
+            Button(action: {
+                spotifyShuffle()
+            })
+            {
+                
+                Image(systemName: ShuffleIcon)
+                    .imageScale(.large)
+                    .font(.system(size: 18))
+                    .foregroundStyle(.secondary)
+                    .frame(width: 30, height: 30)
+                
+            }
+            .background(Color.clear)
+            .buttonStyle(BorderlessButtonStyle())
+            .padding(.horizontal, 17)
+            
+            
+            
+            
+            //Skip backward
+            Button(action: {
+                spotifyLastTrack()
+            }) {
+                Image(systemName: "backward.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.primary)
+                    .font(.system(size: 17))
+                    .frame(width: 30, height: 30)
+                
+            }
+            .background(Color.clear)
+            .buttonStyle(BorderlessButtonStyle())
+            .padding(.horizontal, 5)
+            
+            
+            
+            //Pause
+            Button(action: {
+                spotifyPlayPause()
+            }) {
+                Image(systemName: PlayIcon)
+                    .imageScale(.large)
+                    .foregroundStyle(.primary)
+                    .font(.system(size: 22, weight: .bold))
+                    .frame(width: 30, height: 30)
+                
+            }
+            .background(Color.clear)
+            .buttonStyle(BorderlessButtonStyle())
+            .padding(.horizontal, 16)
+            
+            
+            //Skip forward
+            Button(action: {
+                spotifyNextTrack()
+            }) {
+                Image(systemName: "forward.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.primary)
+                    .font(.system(size: 17))
+                    .frame(width: 30, height: 30)
+                
+            }
+            .background(Color.clear)
+            .buttonStyle(BorderlessButtonStyle())
+            .padding(.horizontal, 5)
+            
+            
+            //Speaker
+            Image(systemName: deviceIcon)
+                .imageScale(.large)
+                .foregroundStyle(.secondary)
+                .font(.system(size: 17))
+                .frame(width: 30, height: 30)
+                .padding(.horizontal, 17)
+            
+        }
+    }
+}
+
+#Preview {
+    ButtonView()
+}
