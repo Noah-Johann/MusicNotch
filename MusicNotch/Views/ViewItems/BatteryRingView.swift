cab25d598fe3ced7e8776dce42154489f7d68293
Noah <179856882+Noah-Johann@users.noreply.github.com>
2025-11-24 16:26:32 +0100

    Moved assets

---
+++ MusicNotch/Views/ViewItems/BatteryRingView.swift
@@ -0,0 +1,18 @@
+//
+//  BatteryRingView.swift
+//  MusicNotch
+//
+//  Created by Noah Johann on 24.11.25.
+//
+
+import SwiftUI
+
+struct BatteryRingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BatteryRingView()
+}
