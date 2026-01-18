41b30529578e0f4e6a56ba7fa01a43b81af581f4
Noah <179856882+Noah-Johann@users.noreply.github.com>
2026-01-18 20:50:41 +0100

    Remove shuffle button on now playing

--- MusicNotch/Views/Buttons/PlayerButtonView.swift
+++ MusicNotch/Views/Buttons/PlayerButtonView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Defaults
 
 
 struct PlayerButtonView: View {
@@ -18,7 +19,7 @@ struct PlayerButtonView: View {
         HStack {
             HoverEffectButton(icon: "shuffle", iconColor: .secondary, iconSize: 24, effectSize: 52, cornerRadius: 17, dot: $musicManager.music.shuffle) {
                 MusicActions.toggleShuffle()
-            }
+            } .opacity(Defaults[.musicPlayer] == .nowPlaying ? 0 : 1)
             
             
             HoverEffectButton(icon: musicManager.music.type == .podcast ? "15.arrow.trianglehead.counterclockwise" : "backward.fill", iconSize: 25, effectSize: 52, cornerRadius: 17, dot: .constant(false)) {
