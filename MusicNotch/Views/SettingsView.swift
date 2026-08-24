44f3361a0979f1cfe59c9ef93f2aabd822bb06b7
Noah <179856882+Noah-Johann@users.noreply.github.com>
2026-08-24 17:35:20 +0200

    Remove old settings code

--- MusicNotch/Views/SettingsView.swift
+++ MusicNotch/Views/SettingsView.swift
@@ -41,62 +41,10 @@ struct SettingsView: View {
                 .animation(.easeInOut(duration: 0.3), value: hoverBehavior)
                 .animation(.easeInOut(duration: 0.3), value: autoMusicGlance)
                 .animation(.easeInOut(duration: 0.3), value: updateManager.updateState)
-                .animation(.bouncy(duration: 0.2), value: updateManager.updateProgress)
-                .animation(.bouncy(duration: 0.3), value: autoPlayer)
+                .animation(.easeInOut(duration: 0.3), value: updateManager.updateProgress)
+                .animation(.easeInOut(duration: 0.3), value: autoPlayer)
             
         } .jochexUseGlassIfAvailable(true)
-        
-//        LuminareDividedStack {
-//            LuminareSidebar {
-//                LuminareSidebarSection(selection: $settingsTab, items: SettingsTab.generalTabs)
-//                LuminareSidebarSection(selection: $settingsTab, items: SettingsTab.middleTabs)
-//                LuminareSidebarSection(selection: $settingsTab, items: SettingsTab.aboutTabs)
-//            }
-//            .frame(width: 220)
-//            .padding(.top, titleBarPadding)
-//            .luminareBackground()
-//            
-//            LuminarePane {
-//                settingsTab.view()
-//            } header: {
-//                HStack {
-//                    settingsTab.icon
-//                    Text(settingsTab.title).font(.title2)
-//                    Spacer()
-//                } .drawingGroup()
-//            }
-//        }
-//        .luminareTint(overridingWith: .accentColor)
-//        .ignoresSafeArea()
-        
-//        LuminarePane() {
-//            VStack {
-//                SettingsGeneralView()
-//                
-//                SettingsDisplayView()
-//                
-//                SettingsNotchView()
-//                
-//                SettingsGesturesView()
-//                
-//                SettingsMusicView()
-//                
-//                SettingsAppearanceView()
-//                
-//                SettingsMusicGlanceView()
-//                
-//                SettingsExtensionView()
-//                
-//                SettingsLockScreenView()
-//                                
-//                SettingsShortcutsView()
-//                
-//                SettingsAboutView()
-//            }
-//            .padding(.horizontal, 5)
-
-//
-//        }
     }
 }
 #Preview {
